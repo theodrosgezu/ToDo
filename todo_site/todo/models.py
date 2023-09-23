920c6be2293fe04490557c464f0dccc2873d9b1f
theodrosgezu <teddyegezu@gmail.com>
2023-09-23 10:56:27 +0300

    edited models

--- todo_site/todo/models.py
+++ todo_site/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Todo(models.Model):
+    title = model.CharField(max_length=100)
+    details = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
