a94498573225557dee1f8ddbe0901194b71e584a
theodrosgezu <teddyegezu@gmail.com>
2023-09-23 11:41:10 +0300

    edited all

--- todo_site/todo/models.py
+++ todo_site/todo/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
-
+ 
+ 
 class Todo(models.Model):
-    title = model.CharField(max_length=100)
-    details = models.DateTimeField(default=timezone.now)
-    
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+ 
     def __str__(self):
         return self.title
--- todo_site/todo/views.py
+++ todo_site/todo/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
-# Create your views here.
-
+ 
 # import todo form and models
  
 from .forms import TodoForm
 from .models import Todo
-
+ 
+###############################################
+ 
+ 
 def index(request):
-
+ 
     item_list = Todo.objects.order_by("-date")
-    if requeat.method == ""POST":
-        form = Todoform(request.POST)
+    if request.method == "POST":
+        form = TodoForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('todo')
     form = TodoForm()
-    
+ 
     page = {
         "forms": form,
         "list": item_list,
         "title": "TODO LIST",
     }
     return render(request, 'todo/index.html', page)
-
+ 
+ 
 ### function to remove item, it receive todo item_id as primary key from url ##
 def remove(request, item_id):
     item = Todo.objects.get(id=item_id)
